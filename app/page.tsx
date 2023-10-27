2ae546f60a98948bb77c6dd2d33aa0399c7a24de
Franciszek Pawlak <box@franciszekpawlak.pl>
2023-10-27 22:46:42 +0200

    init: next & flowbite

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Alert } from "flowbite-react";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col content-center justify-center w-full h-full p-40">
+      <Alert color="info">Tu będzie apka hehe</Alert>
+    </main>
+  );
+}
